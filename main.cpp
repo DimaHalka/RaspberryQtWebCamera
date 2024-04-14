6643a331f1f5d38451366fcc79cc12624030856b
DimaHalka <dima.halka@gmail.com>
2024-04-14 23:37:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,28 +9,23 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // Open the webcam
     VideoCapture cap(0);
     if (!cap.isOpened()) {
         qDebug() << "Error: Unable to open the webcam";
         return -1;
     }
 
-    // Create a QLabel to display the video stream
     QLabel label;
     label.show();
 
-    // Create a QTimer to update the QLabel with new frames
     QTimer timer;
     QObject::connect(&timer, &QTimer::timeout, [&]() {
         Mat frame;
         cap >> frame; // Capture a frame from the webcam
 
-        // Convert the OpenCV frame to a QImage for display
         QImage qImage(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
         label.setPixmap(QPixmap::fromImage(qImage.rgbSwapped())); // Swapping RGB to BGR
 
-        // Resize the QLabel to fit the video stream
         label.setScaledContents(true);
         label.adjustSize();
     });
